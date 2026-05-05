2b8f1cbadf60d81a0979a746962cc029cdd3b24a
akbarkevin112-jpg <akbarkevin112@gmail.com>
2026-05-05 11:25:10 +0700

    feat: implement unit tests for all user APIs (closes #14)

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import { Elysia } from "elysia";
 import { env } from "./config/env";
 import { userRoutes } from "./routes/users-route";
 
-const app = new Elysia()
+export const app = new Elysia()
   .use(userRoutes)
-  .get("/", () => ({ message: "Belajar Vibe Coding API is running 🚀" }))
-  .listen(env.PORT);
+  .get("/", () => ({ message: "Belajar Vibe Coding API is running 🚀" }));
 
-console.log(
-  `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
-);
+if (process.env.NODE_ENV !== "test") {
+  app.listen(env.PORT);
+  console.log(
+    `🦊 Elysia is running at http://${app.server?.hostname}:${app.server?.port}`
+  );
+}
